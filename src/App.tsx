871b4b5f9b91c68b39f0a4462cc477341ac89612
Milena Moniszka <balletmila@gmail.com>
2018-10-25 22:53:08 +0200

    images fix v2

--- src/App.tsx
+++ src/App.tsx
@@ -26,11 +26,11 @@ class App extends React.Component<any, any> {
   public render() {
     return (
       <div className="App">
-        <header className="parallax">
+        <div className="parallax">
           <div className="App-title-back">
             <div className="App-title">Welcome to the base of most scariest   <span className="blood"> MONSTERS </span>   in the world!</div>
           </div>
-        </header>
+        </div>
         <div className="app-main">
           <MonsterList />
         </div>
