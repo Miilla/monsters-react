5c51d540f09ec6e502069cb9e810543036e68f96
Milena Moniszka <balletmila@gmail.com>
2018-11-01 12:23:26 +0100

    local serv

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
  * API root url
  */
 
-export const STAT_API = 'https://monster-app.herokuapp.com';
-// export const STAT_API = 'http://localhost:5000';
+// export const STAT_API = 'https://monster-app.herokuapp.com';
+export const STAT_API = 'http://localhost:8080';
 
 export const API_URL = STAT_API + '/api/v1/';
